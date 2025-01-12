b6e8d339b8c17c9ae1b55971c3de010994ab2dcd
jherold736 <jherold736@gmail.com>
2025-01-12 23:59:16 +0100

    wszystkie funkcjonalności do 2:51, potem wywaliło error XD

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -11,6 +11,7 @@ import { currentUser } from '@clerk/nextjs/server';
 import Image from 'next/image';
 import { updateDocument } from '@/lib/actions/room.actions';
 import Loader from './Loader';
+import ShareModal from './ShareModal';
 
 const CollaborativeRoom = ({roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
 
@@ -105,6 +106,15 @@ const CollaborativeRoom = ({roomId, roomMetadata, users, currentUserType }: Coll
                      </div>
                      <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
                         <ActiveCollaborators />
+
+                        <ShareModal 
+                          roomId={roomId}
+                          collaborators={users}
+                          creatorId={roomMetadata.creatorId}
+                          currentUserType={currentUserType}
+                        />  
+
+
                         <SignedOut>
                           <SignInButton />
                         </SignedOut>
