e80e6f72affb79913504cbc713693576c23bdd21
jherold736 <jherold736@gmail.com>
2025-01-12 19:43:34 +0100

    mention someone function @

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -15,6 +15,7 @@ import { FloatingComposer, FloatingThreads, liveblocksConfig, LiveblocksPlugin, 
 import FloatingToolbarPlugin from './plugins/FloatingToolbarPlugin'
 import Loader from '../Loader';
 import { useThreads } from '@liveblocks/react/suspense';
+import Comments from '../Comments';
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
@@ -64,10 +65,10 @@ export function Editor({ roomId, currentUserType }: { roomId: string, currentUse
           )}
 
           <LiveblocksPlugin>
-          <FloatingComposer className="w-[350px]" />
-          <FloatingThreads threads={threads} />
-          </LiveblocksPlugin>
-          
+            <FloatingComposer className="w-[350px]" />
+            <FloatingThreads threads={threads} />
+            <Comments />
+          </LiveblocksPlugin>         
         </div>
       </div>
     </LexicalComposer>
