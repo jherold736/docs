dd6d971b62936894f510bd39e153e09ccf1b70bb
jherold736 <jherold736@gmail.com>
2025-01-12 15:11:07 +0100

    untitled napis, drobnezmiany w collaborativeRoom

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -5,15 +5,33 @@ import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import ActiveCollaborators from './ActiveCollaborators';
+import { useRef, useState } from 'react';
+import { Input } from './ui/input';
 
 const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
+
+  const [documentTitle, setdocumentTitle] = useState(roomMetadata.title);
+  const [editing, setEditing] = useState(false);
+  const [loading,setLoading] = useState(false)  
+  
+  const containerRef = useRef<HTMLDivElement>(null);
+  const  inputRef = useRef<HTMLDivElement>(null);
+
   return (
     <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<div>Loading…</div>}>
        <div className="collaborative-room">
                  <Header>
-                     <div className="flex w-fit items-center justify-center gap-2">
-                         <p className="document-tittle">Share</p>
+                     <div ref={containerRef} className="flex w-fit items-center justify-center gap-2">
+                       {editing && !loading? (
+                        <Input
+                        
+                        />
+                       ) : (
+                        <>
+                          <p className="document-title">{documentTitle}</p>
+                        </>
+                       )}  
                      </div>
                      <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
                         <ActiveCollaborators />
